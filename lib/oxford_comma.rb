8a019d0d85c401599b0f4808ca8347efb982144d
jee <jag918@nyu.edu>
2018-05-08 00:55:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   if array.size==1 
-    string=array.split(" ")
+    string=array.split("\n")
   elsif array.size==2 
     string=array.split(",")
     string="#{string[0]} and #{string[1]}"
