b999276229a6826a92ffbeafd464d2c10b340e5b
jee <jag918@nyu.edu>
2018-05-08 00:49:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,23 @@
 def oxford_comma(array)
-  string=array.split()
-  return string
-
+  if array.size==1 
+    string=array.split(" ")
+    return string
+  elsif array.size==2 
+    string=array.split(,)
+    string=string[0]+"and"+string[1]
+  elsif array.size==3
+    string=array.split(,)
+    string=string[0]+", "+string[1]+",and "string[2]
+  else
+    string=array.split(,)
+    counter=0
+    loop do
+      counter+=1
+      string+=string[counter]+", " 
+      if counter < array.size
+        string+=" ,and "string[counter]
+        break
+      end
+    end
+  end
 end
