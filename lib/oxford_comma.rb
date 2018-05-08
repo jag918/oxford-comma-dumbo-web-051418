350b33c871f08024e2d4dc7c6a1d5d979c5e6cda
jee <jag918@nyu.edu>
2018-05-08 01:09:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,20 +1,12 @@
 def oxford_comma(array)
   if array.size==1 
-    string=array.split()
+    string=array.join()
   elsif array.size==2 
-    string=array.split(",")
-    new_string="#{string[0]} and #{string[1]}"
+    string=array.join(" and ")
   else
-    string=array.split(",")
-    counter=0
-    loop do
-      counter+=1
-      new_string+="#{string[counter]}, " 
-      if counter < array.size
-        new_string+=" ,and #{string[counter]}"
-        break
-      end
-    end
+    string=array.join(",")
+    
+
   end
   puts new_string
 end
