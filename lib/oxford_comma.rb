a99e04eec865349214a5b230b0096161c99d4f16
jee <jag918@nyu.edu>
2018-05-08 01:39:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size<=1 
+    string=array.join()
+  elsif array.size<=2 
+    string=array.join(" and ")
+  else
+      last_chr=array.pop
+      array.push("and")
+      string=array.join(", ")
+      string+=" #{last_chr}"
+  end
+  string
 end
